772d1fffcf2b1fcbfbc46c1c9ef30f7b9618e688
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-21 14:52:05 -0700

    test for exercise four passes

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,17 @@
 -- find all customers first_name, last_name, address, and city
+SELECT
+    customer.first_name,
+    customer.last_name,
+    address.address,
+    city.city
+FROM
+    customer
 -- Similar to exercise 3 but with customer, address, city
+LEFT JOIN
+    address
+ON
+    customer.address_id = address.address_id
+LEFT JOIN
+    city
+ON 
+    address.city_id = city.city_id;

e472fd99ac1b74e32e9f072d7f2e713dbd2e547e
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-03-21 12:59:32 -0700

    worked on exercise 3

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,17 @@
 -- find all film titles with the category Action
+SELECT
+    film.title
+FROM    
+    film
 -- use a LEFT JOIN FROM film onto film_category
+LEFT JOIN
+    film_category
+ON
+    film.film_id = film_category.film_id;
 -- use LEFT JOIN FROM film_categor onto category
+LEFT JOIN
+    category
+ON
+    film_category.category_id = category.category_id
+WHERE
+    category.name = 'Action';
